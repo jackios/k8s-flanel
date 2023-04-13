fbbec7980fa25e49c5afec1db4535d21ded23a96
jackios <46483891+jackios@users.noreply.github.com>
2023-04-13 20:54:08 +0800

    Update setup.sh
    k8 in docker  1control 3 worker

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 # by jackios
 # desc  create  acluster
 # 1. pre  noCNI env
+
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-amd64 && chmod +x ./kind && sudo mv ./kind /usr/local/bin/kind
 cat <<EOF | kind create cluster  --image=kindest/node:v1.23.4 --config=-
 kind: Cluster
 name: flanel-dup
