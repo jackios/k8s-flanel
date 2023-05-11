95d906163f0e4531bd02c1789747f571cd1f3c7a
WX0lqlyy <GESsKQqB2V6awLEl3PC6>
2023-05-11 15:37:32 +0800

    edit  name of   cluster

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-amd64 && chmod +x ./kind && sudo mv ./kind /usr/local/bin/kind
 cat <<EOF | kind create cluster  --image=kindest/node:v1.23.4 --config=-
 kind: Cluster
-name: flanel-dup
+name: flanel-flannel
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
   disableDefaultCNI: true
@@ -21,7 +21,7 @@ nodes:
 - role: worker
 EOF
 
-# 2. remove taints 
+# 2. remove taints
 con_no=`kubectl  get  no|awk 'NR>1{print $1}'|grep  control-plane`
 kubectl  taint  no $con_no  node-role.kubernetes.io/master-
 kubectl  get  no  -o wide
